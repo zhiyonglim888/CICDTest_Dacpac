24543ba79369b0f2e4cf34303f97ad5c84d1aadd
Jaeden <zhiyong.lim@appfuxion.com>
2022-11-06 13:35:11 +0800

    asdsadas

--- CICDTest_Dacpac/Script.PostDeployment.sql
+++ CICDTest_Dacpac/Script.PostDeployment.sql
@@ -1,6 +1,6 @@
-﻿:setvar path "\dbo\Stored Procedures\"
+﻿:setvar path ".\dbo\Stored Procedures\"
 
 :r .\dbo\ExternalFolder\ExternalDbo.sql.
 :r .\dbo\ExternalFolder\ExternalNestedFolder\ExternalNestedDbo.sql.
-:r .$(path)"DefaultStoreProcFolder.sql".
+:r $(path)"DefaultStoreProcFolder.sql".
 :r .\TestOutsideDbo\*.sql.
