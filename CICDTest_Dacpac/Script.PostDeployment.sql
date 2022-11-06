4285b6fb2219f6060d5d8c42892ad340878c5aa7
Jaeden <zhiyong.lim@appfuxion.com>
2022-11-06 13:24:30 +0800

    sss

--- CICDTest_Dacpac/Script.PostDeployment.sql
+++ CICDTest_Dacpac/Script.PostDeployment.sql
@@ -1,4 +1,4 @@
 ﻿:r .\dbo\ExternalFolder\ExternalDbo.sql.
 :r .\dbo\ExternalFolder\ExternalNestedFolder\ExternalNestedDbo.sql.
-:r ".\dbo\Stored Procedures\DefaultStoreProcFolder.sql."
+:r ".\dbo\Stored Procedures\DefaultStoreProcFolder.sql".
 :r .\TestOutsideDbo\*.sql.
