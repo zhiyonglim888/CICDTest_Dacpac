d983295453147b1b694f24aa45250692f150e533
Jaeden <zhiyong.lim@appfuxion.com>
2022-11-07 11:57:06 +0800

    sadasdsad

---
+++ CICDTest_Dacpac/TestOutsideDbo1/OutsideDbo1_1/OutsideDbo1_1_1/OutsideDbo1_1_!.sql
@@ -0,0 +1,10 @@
+IF NOT EXISTS (
+  SELECT * 
+  FROM   sys.columns 
+  WHERE  object_id = OBJECT_ID(N'[dbo].[UserProfile]') 
+         AND name = 'OutsideDbo1_1_1'
+)
+BEGIN
+  ALTER TABLE UserProfile
+    ADD OutsideDbo1_1_1 varchar(255)
+END;
